d3ac19917a42743125e8ab30d9a93668c500bf1f
a.snegovskii <CarrionGrow@Gmail.com>
2024-03-30 19:57:51 +0300

    test

--- internal/services/server/middleware.go
+++ internal/services/server/middleware.go
@@ -57,10 +57,6 @@ func (s *Server) logMiddleware(next http.Handler) http.Handler {
 
 func (s *Server) JSONContentTypeMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Content-Type") != "application/json" {
-			http.Error(w, "bad request", http.StatusBadRequest)
-			return
-		}
 		next.ServeHTTP(w, r)
 	})
 }
