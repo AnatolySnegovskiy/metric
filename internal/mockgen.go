671e50777eee0b86beb533dbc1898be76c1ef217
a.snegovskii <CarrionGrow@Gmail.com>
2024-04-13 15:10:16 +0300

    add tests

--- internal/mockgen.go
+++ internal/mockgen.go
@@ -3,4 +3,3 @@ package internal
 //go:generate mockgen -source=storages/mem_storage.go -destination=mocks/mem_storage_mock.go -package=mocks
 //go:generate mockgen -source=services/agent/agent.go -destination=mocks/agent_mock.go -package=mocks
 //go:generate mockgen -source=services/agent/storage.go -destination=mocks/storage_mock.go -package=mocks
-//go:generate mockgen -source=storages/clients/postgres.go -destination=mocks/postgres_mock.go -package=mocks
