3b4acc9d31d5b7d495f44de2bc37e93404e472d2
a.snegovskii <CarrionGrow@Gmail.com>
2024-04-21 15:12:31 +0300

    PR fix

--- internal/storages/migrations/001_init.sql
+++ internal/storages/migrations/001_init.sql
@@ -1,8 +1,8 @@
 -- Write your migrate up statements here
-CREATE TABLE IF NOT EXISTS counter (name varchar(255) PRIMARY KEY, value int8);
-CREATE TABLE IF NOT EXISTS gauge (name varchar(255) PRIMARY KEY, value DOUBLE PRECISION);
+CREATE TABLE counter (name varchar(255) PRIMARY KEY, value int8);
+CREATE TABLE gauge (name varchar(255) PRIMARY KEY, value DOUBLE PRECISION);
 ---- create above / drop below ----
-DROP TABLE IF EXISTS counter;
-DROP TABLE IF EXISTS gauge;
+DROP TABLE counter;
+DROP TABLE gauge;
 -- Write your migrate down statements here. If this migrations is irreversible
 -- Then delete the separator line above.
