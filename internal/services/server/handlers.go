82ab966272acfd660dc15da642dbb22f21ad0474
a.snegovskii <CarrionGrow@Gmail.com>
2024-06-25 18:40:08 +0300

    fix tests

--- internal/services/server/handlers.go
+++ internal/services/server/handlers.go
@@ -220,41 +220,37 @@ func (s *Server) writeMassPostMetricHandler(rw http.ResponseWriter, req *http.Re
 		return
 	}
 
-	storage := s.storage
+	list := make(map[string]map[string]float64)
 
 	for _, metricDTO := range *metricDTOCollection {
+		if list[metricDTO.MType] == nil {
+			list[metricDTO.MType] = make(map[string]float64)
+		}
+
 		if metricDTO.Delta != nil {
-			matric, err := storage.GetMetricType(metricDTO.MType)
-			if err != nil {
-				rw.WriteHeader(http.StatusNotFound)
-				fmt.Fprintf(rw, "%v", fmt.Sprintf(`{"error":"metric type %s not found"}`, metricDTO.MType))
-				return
-			}
-
-			metricValue := *metricDTO.Delta
-			if metricValue != 0 {
-				if err := matric.ProcessMassive(req.Context(), map[string]float64{metricDTO.ID: float64(metricValue)}); err != nil {
-					rw.WriteHeader(http.StatusInternalServerError)
-					fmt.Fprintf(rw, "%v", fmt.Sprintf(`{"error":"%s"}`, err.Error()))
-					return
-				}
-			}
+			list[metricDTO.MType][metricDTO.ID] += float64(*metricDTO.Delta)
 		} else if metricDTO.Value != nil {
-			matric, err := storage.GetMetricType(metricDTO.MType)
-			if err != nil {
-				rw.WriteHeader(http.StatusNotFound)
-				fmt.Fprintf(rw, "%v", fmt.Sprintf(`{"error":"metric type %s not found"}`, metricDTO.MType))
-				return
-			}
+			list[metricDTO.MType][metricDTO.ID] = *metricDTO.Value
+		}
+	}
 
-			metricValue := *metricDTO.Value
-			if metricValue != 0 {
-				if err := matric.ProcessMassive(req.Context(), map[string]float64{metricDTO.ID: float64(metricValue)}); err != nil {
-					rw.WriteHeader(http.StatusInternalServerError)
-					fmt.Fprintf(rw, "%v", fmt.Sprintf(`{"error":"%s"}`, err.Error()))
-					return
-				}
-			}
+	storage := s.storage
+
+	for metricType, metric := range list {
+		matric, err := storage.GetMetricType(metricType)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(rw, "%v", fmt.Sprintf(`{"error":"metric type %s not found"}`, metricType))
+			return
+		}
+
+		err = matric.ProcessMassive(req.Context(), metric)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(rw, "%v", fmt.Sprintf(`{"error":"%s"}`, err.Error()))
+			return
 		}
 	}
 
